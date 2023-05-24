87ccfd6e5d8215f39ac5b658af43d1d36f2581da
estebangaviria2006 <munozleandro343@gmail.com>
2023-05-24 16:27:11 -0500

    add fuentes

---
+++ js/menu-subitems.js
@@ -0,0 +1,11 @@
+let  botonMenu = document.getElementById('btnmenu');
+
+botonMenu.addEventListener('click', function() {
+    alert("Gracias por su Atencion");
+})
+
+let nosotros=document.getElementById('we');
+
+nosotros.addEventListener('click', function(){
+    alert("Somos excelentes");
+})
