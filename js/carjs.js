fd986d715a8020f5052267a113edb08c1daf3340
estebangaviria2006 <munozleandro343@gmail.com>
2023-10-18 16:44:26 -0500

    add fuentes

--- js/carjs.js
+++ js/carjs.js
@@ -2,54 +2,44 @@ let imgBox = document.getElementById("imgBox");
 let imgMini1 = document.getElementById("imgMini1");
 let imgMini2 = document.getElementById("imgMini2");
 let imgMini3 = document.getElementById("imgMini3");
-let imgMini4 = document.getElementById("imgMini4");
+
 
 imgMini1.addEventListener("click",function(){
-    imgBox.setAttribute("src","carrito/img/image-product-1.jpg")
+    imgBox.setAttribute("src","img/XTZ 250-BLANCA.png")
     boxImgBox1.classList.add("box-img-mini")
     boxImgBox2.classList.remove("box-img-mini")
     boxImgBox3.classList.remove("box-img-mini")
-    boxImgBox4.classList.remove("box-img-mini")
+   
     boxImgBox1.classList.add("img-mini-act")
     boxImgBox2.classList.remove("img-mini-act")
     boxImgBox3.classList.remove("img-mini-act")
-    boxImgBox4.classList.remove("img-mini-act")
+   
 })
 
 imgMini2.addEventListener("click",function(){
-    imgBox.setAttribute("src","carrito/img/image-product-2.jpg")
+    imgBox.setAttribute("src","img/XTZ 250 NEGRA.png")
     boxImgBox2.classList.add("box-img-mini")
     boxImgBox1.classList.remove("box-img-mini")
     boxImgBox3.classList.remove("box-img-mini")
-    boxImgBox4.classList.remove("box-img-mini")
+   
     boxImgBox2.classList.add("img-mini-act")
     boxImgBox1.classList.remove("img-mini-act")
     boxImgBox3.classList.remove("img-mini-act")
-    boxImgBox4.classList.remove("img-mini-act")
+    
 })
 
 imgMini3.addEventListener("click",function(){
-    imgBox.setAttribute("src","carrito/img/image-product-3.jpg")
+    imgBox.setAttribute("src","img/xtz 250-azul.png")
     boxImgBox3.classList.add("box-img-mini")
     boxImgBox1.classList.remove("box-img-mini")
     boxImgBox2.classList.remove("box-img-mini")
-    boxImgBox4.classList.remove("box-img-mini")
+   
     boxImgBox3.classList.add("img-mini-act")
     boxImgBox1.classList.remove("img-mini-act")
     boxImgBox2.classList.remove("img-mini-act")
-    boxImgBox4.classList.remove("img-mini-act")
+    
 })
-imgMini4.addEventListener("click",function(){
-    imgBox.setAttribute("src","carrito/img/image-product-4.jpg")
-    boxImgBox4.classList.add("box-img-mini")
-    boxImgBox1.classList.remove("box-img-mini")
-    boxImgBox2.classList.remove("box-img-mini")
-    boxImgBox3.classList.remove("box-img-mini")
-    boxImgBox4.classList.add("img-mini-act")
-    boxImgBox1.classList.remove("img-mini-act")
-    boxImgBox2.classList.remove("img-mini-act")
-    boxImgBox3.classList.remove("img-mini-act")
-})
+
 
 let btnmenos = document.getElementById("menos");
 let canBox = document.getElementById("cantBox");
@@ -57,7 +47,7 @@ let btnCantidad = document.getElementById("canOb");
 let btnmas =document.getElementById("mas");
 let numElem = document.getElementById("numeroElementos");
 
-let valorUnitario = 199000;
+let valorUnitario = 26000000;
 let valorUnitarioTxt = valorUnitario.toString();
 
 let valoeUnitarioPeso = new Intl.NumberFormat().format(valorUnitario);
@@ -96,7 +86,7 @@ btnmenos.addEventListener("click", function(){
         numElem.innerHTML= totalElementosTxt;
         valorTotalInt = valorUnitario*totalElementosInt;
         valorTotal.innerHTML = "$ " + new Intl.NumberFormat().format(valorTotalInt);
-        if (valorTotalInt<=0){
+        if (valorTotalInt=0){
             valorTotal.innerHTML = "$ " + new Intl.NumberFormat().format(valorUnitario);
         }
     }
@@ -121,59 +111,49 @@ let imgEscPrin = document.getElementById("imgEscPrin");
 let imgMiniEsc1 = document.getElementById("imgMiniEsc1");
 let imgMiniEsc2 = document.getElementById("imgMiniEsc2");
 let imgMiniEsc3 = document.getElementById("imgMiniEsc3");
-let imgMiniEsc4 = document.getElementById("imgMiniEsc4");
+
 
 imgMiniEsc1.addEventListener("click",function(){
-    imgEscPrin.setAttribute("src","carrito/img/image-product-1.jpg");
+    imgEscPrin.setAttribute("src","img/XTZ 250-BLANCA.png");
     imgEsc1.classList.add("box-img-mini");
     imgEsc2.classList.remove("box-img-mini");
     imgEsc3.classList.remove("box-img-mini");
-    imgEsc4.classList.remove("box-img-mini");
+   
     imgEsc1.classList.add("img-mini-act");
     imgEsc2.classList.remove("img-mini-act");
     imgEsc3.classList.remove("img-mini-act");
-    imgEsc4.classList.remove("img-mini-act");
+   
 })
 
 imgMiniEsc2.addEventListener("click",function(){
-    imgEscPrin.setAttribute("src","carrito/img/image-product-2.jpg")
+    imgEscPrin.setAttribute("src","img/XTZ 250 NEGRA.png")
     imgEsc2.classList.add("box-img-mini")
     imgEsc1.classList.remove("box-img-mini")
     imgEsc3.classList.remove("box-img-mini")
-    imgEsc4.classList.remove("box-img-mini")
+   
     imgEsc2.classList.add("img-mini-act")
     imgEsc1.classList.remove("img-mini-act")
     imgEsc3.classList.remove("img-mini-act")
-    imgEsc4.classList.remove("img-mini-act")
+ 
 })
 
 imgMiniEsc3.addEventListener("click",function(){
-    imgEscPrin.setAttribute("src","carrito/img/image-product-3.jpg")
+    imgEscPrin.setAttribute("src","img/xtz 250-azul.png")
     imgEsc3.classList.add("box-img-mini")
     imgEsc1.classList.remove("box-img-mini")
     imgEsc2.classList.remove("box-img-mini")
-    imgEsc4.classList.remove("box-img-mini")
+   
     imgEsc3.classList.add("img-mini-act")
     imgEsc1.classList.remove("img-mini-act")
     imgEsc2.classList.remove("img-mini-act")
-    imgEsc4.classList.remove("img-mini-act")
+   
 })
-imgMiniEsc4.addEventListener("click",function(){
-    imgEscPrin.setAttribute("src","carrito/img/image-product-4.jpg")
-    imgEsc4.classList.add("box-img-mini")
-    imgEsc1.classList.remove("box-img-mini")
-    imgEsc2.classList.remove("box-img-mini")
-    imgEsc3.classList.remove("box-img-mini")
-    imgEsc4.classList.add("img-mini-act")
-    imgEsc1.classList.remove("img-mini-act")
-    imgEsc2.classList.remove("img-mini-act")
-    imgEsc3.classList.remove("img-mini-act")
-})
+
 
 let btnAtras = document.getElementById("btnAtras");
 let btnAdelante = document.getElementById("btnAdelante");
 let counter=0;
-let numImg=["carrito/img/image-product-1.jpg","carrito/img/image-product-2.jpg","carrito/img/image-product-3.jpg","carrito/img/image-product-4.jpg"];
+let numImg=["img/XTZ 250-BLANCA.png","img/XTZ 250 NEGRA.png","img/xtz 250-azul.png"];
 
 btnAtras.addEventListener("click", function(){
     if (counter>0) {
@@ -187,44 +167,35 @@ btnAtras.addEventListener("click", function(){
         imgEsc1.classList.add("box-img-mini");
         imgEsc2.classList.remove("box-img-mini");
         imgEsc3.classList.remove("box-img-mini");
-        imgEsc4.classList.remove("box-img-mini");
+        
         imgEsc1.classList.add("img-mini-act");
         imgEsc2.classList.remove("img-mini-act");
         imgEsc3.classList.remove("img-mini-act");
-        imgEsc4.classList.remove("img-mini-act");
+        
     } else if(counter==1) {
         imgEsc2.classList.add("box-img-mini")
         imgEsc1.classList.remove("box-img-mini")
         imgEsc3.classList.remove("box-img-mini")
-        imgEsc4.classList.remove("box-img-mini")
+      
         imgEsc2.classList.add("img-mini-act")
         imgEsc1.classList.remove("img-mini-act")
         imgEsc3.classList.remove("img-mini-act")
-        imgEsc4.classList.remove("img-mini-act")
+       
     } else if (counter==2) {
         imgEsc3.classList.add("box-img-mini")
         imgEsc1.classList.remove("box-img-mini")
         imgEsc2.classList.remove("box-img-mini")
-        imgEsc4.classList.remove("box-img-mini")
+        
         imgEsc3.classList.add("img-mini-act")
         imgEsc1.classList.remove("img-mini-act")
         imgEsc2.classList.remove("img-mini-act")
-        imgEsc4.classList.remove("img-mini-act")
-    } else {
-        imgEsc4.classList.add("box-img-mini")
-        imgEsc1.classList.remove("box-img-mini")
-        imgEsc2.classList.remove("box-img-mini")
-        imgEsc3.classList.remove("box-img-mini")
-        imgEsc4.classList.add("img-mini-act")
-        imgEsc1.classList.remove("img-mini-act")
-        imgEsc2.classList.remove("img-mini-act")
-        imgEsc3.classList.remove("img-mini-act")
-    }
+        
+    } 
 
 })
 
 btnAdelante.addEventListener("click", function(){
-    if (counter<3) {
+    if (counter<2) {
        counter++;
     } else {
         counter =0;
@@ -235,39 +206,30 @@ btnAdelante.addEventListener("click", function(){
     imgEsc1.classList.add("box-img-mini");
     imgEsc2.classList.remove("box-img-mini");
     imgEsc3.classList.remove("box-img-mini");
-    imgEsc4.classList.remove("box-img-mini");
+   
     imgEsc1.classList.add("img-mini-act");
     imgEsc2.classList.remove("img-mini-act");
     imgEsc3.classList.remove("img-mini-act");
-    imgEsc4.classList.remove("img-mini-act");
+    
 } else if(counter==1) {
     imgEsc2.classList.add("box-img-mini")
     imgEsc1.classList.remove("box-img-mini")
     imgEsc3.classList.remove("box-img-mini")
-    imgEsc4.classList.remove("box-img-mini")
+   
     imgEsc2.classList.add("img-mini-act")
     imgEsc1.classList.remove("img-mini-act")
     imgEsc3.classList.remove("img-mini-act")
-    imgEsc4.classList.remove("img-mini-act")
+   
 } else if (counter==2) {
     imgEsc3.classList.add("box-img-mini")
     imgEsc1.classList.remove("box-img-mini")
     imgEsc2.classList.remove("box-img-mini")
-    imgEsc4.classList.remove("box-img-mini")
+    
     imgEsc3.classList.add("img-mini-act")
     imgEsc1.classList.remove("img-mini-act")
     imgEsc2.classList.remove("img-mini-act")
-    imgEsc4.classList.remove("img-mini-act")
-} else {
-    imgEsc4.classList.add("box-img-mini")
-    imgEsc1.classList.remove("box-img-mini")
-    imgEsc2.classList.remove("box-img-mini")
-    imgEsc3.classList.remove("box-img-mini")
-    imgEsc4.classList.add("img-mini-act")
-    imgEsc1.classList.remove("img-mini-act")
-    imgEsc2.classList.remove("img-mini-act")
-    imgEsc3.classList.remove("img-mini-act")
-}
+    
+} 
 })
 
 
